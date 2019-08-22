e2ab47e58cb0cb3ab821810f35220972e26d63b1
christian@baltzer.de <christian@baltzer.de>
2019-08-22 23:18:24 +0200

    Initial Commit

---
+++ MPS-FanProjekt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MPS-FanProjekt
+//
+//  Created by Christian Baltzer on 22.08.19.
+//  Copyright © 2019 Christian Baltzer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
