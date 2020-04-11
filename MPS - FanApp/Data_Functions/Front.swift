e32b6afe843712bc91b232f5ed9c1d6a584868d2
Christian - Darker97 <christian@baltzer.de>
2020-04-11 19:13:00 +0200

    Erstellte Grundstruktur

---
+++ MPS - FanApp/Data_Functions/Front.swift
@@ -0,0 +1,27 @@
+//
+//  Front.swift
+//  MPS - FanApp
+//
+//  Created by Christian Baltzer on 11.04.20.
+//  Copyright © 2020 Christian Baltzer. All rights reserved.
+//
+
+import Foundation
+
+func LoadData(){
+    
+    //Laden Main Seite
+    
+    // Scrappen der Termine
+    // Selector     -> "#linkTermine > ul > li > a"
+    
+    // Laden aller Feste
+    
+    // Laden der Infos
+        // Bands
+        // Heerlager
+        // Anfahrt
+        // Marktsände
+        
+    
+}
