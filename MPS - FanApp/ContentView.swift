cdcd5a5f28b479ed55319f99013dddf7ede174c0
Christian - Darker97 <christian@baltzer.de>
2020-04-11 18:35:31 +0200

    Initial Commit

---
+++ MPS - FanApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MPS - FanApp
+//
+//  Created by Christian Baltzer on 11.04.20.
+//  Copyright © 2020 Christian Baltzer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
