d7b12d4816af6c6dc6a8138cf33c2ea2c2c5bb96
Christian - Darker97 <christian@baltzer.de>
2020-04-12 12:17:13 +0200

    added Front Functions

--- MPS - FanApp/Data_Functions/Front.swift
+++ MPS - FanApp/Data_Functions/Front.swift
@@ -9,19 +9,109 @@
 import Foundation
 
 func LoadData(){
+    let db = openDatabase()
     
-    //Laden Main Seite
-    
-    // Scrappen der Termine
-    // Selector     -> "#linkTermine > ul > li > a"
-    
-    // Laden aller Feste
-    
-    // Laden der Infos
-        // Bands
-        // Heerlager
-        // Anfahrt
-        // Marktsände
+    // Fest Daten
+    var MainLink = "https://www.spectaculum.de/"
+
+    // Main Seite
+    var Main_Html = Scrapper.laden_Websites(MainLink)
+
+    var FestNamen = Scrapper.scrapper_Objecte_Text(Main_Html, "#linkTermine > ul > li > a")
+    var FestLinks = Scrapper.scrapper_Objecte_Links(Main_Html, "#linkTermine > ul > li > a")
+
+
+    // ----------------------------------------------------------------- //
+    // Feste
+    var i = 0
+    for Fest in FestLinks{
+        var link = ""
+        var Sucher = ""
+        
+        var Fest_InfoText = []
+        var Fest_Name = ""
+        var Fest_Anfahrt = ""
+        
+        var Datum = ""
+        
+        
+        link = MainLink + Fest
+        Sucher = "#main > p"
+        Fest_InfoText = Scrapper.scrapper_Objecte_Text(Scrapper.laden_Websites(link), Sucher)
+        
+        Fest_Name = FestNamen[i]
+        
+        link = MainLink + Fest + "/anfahrt.php"
+        Sucher = "#main > p"
+        Fest_Anfahrt = Scrapper.scrapper_Objecte_Text(Scrapper.laden_Websites(link), Sucher)[0]
+        
+        //Einfügen
+        insert_Fest(db, Fest_Name, link, Fest_InfoText, Datum, Fest_Anfahrt)
+        
+        
+        // ----------------------------------------------------------------- //
+        //Tage & Bands
+        link = MainLink + FestLink + "/kuenstler.php"
+        Sucher = "#main > p"
+        var Tage_Namen = Scrapper.scrapper_Objecte_Text(link, Sucher)
+        var Tage_Links = Scrapper.scrapper_Objecte_Links(link, Sucher)
+        
+        var Fest_BandNamen = []
+        var t = 0
+        for tage in Tage_Links{
+            var TagesHtml = laden_Websites(link: tage)
+            // Typ
+            Sucher = ".kategorie ui-helper-clearfix"
+            var kategorien_HTML = scrapper_Objecte(html: TagesHtml, Selector: Sucher)
+            
+            for kategorie in kategorien_HTML{
+                Selector = "H2"
+                kategorie_Name = scrapper_Objecte_Text(html: kategorie, Selector: Selector)[0]
+                Künsteler = scrapper_Objecte(html: kategorie, Selector: ".container")
+                
+                for Kunst in Künsteler{
+                    KunstlerName = scrapper_Objecte_Text(html: Kunst, Selector: ".kuenstler_name")[0]
+                    KunstlerLink = nil
+                    KunstlerLink = try scrapper_Objecte_Links(html: Kunst, Selector: ".kontakt_daten")[0]
+                    
+                    // Einfügen
+                    insert_Band(db, KunstlerName, katejorie_Name, Tage_Name[t], KunstlerLink)
+                }
+                
+            }
+
+            t = t+1
+        }
+        
+        // ----------------------------------------------------------------- //
+        //Markt
+        link = MainLink + FestLink + "/markthaendler.php"
+        Sucher = "#tabTeilnehmendeMarkthaendler > container"
+        
+        var MarktstandHTML = Scrapper.laden_Websites(link)
+        var MarktStände = Scrapper.scrapper_Objecte_Text(MarktstandHTML, Sucher)
+        
+        for Markt in MarktStände{
+            var Markt_Namen = ""
+            var Markt_Kontakt = ""
+            var Markt_Homepage = ""
+            
+            Markt_Namen         = Scrapper.scrapper_Objecte_Text(Markt, "")
+            Markt_Kontakt       = Scrapper.scrapper_Objecte_Text(Markt, "")
+            Markt_Homepage      = Scrapper.scrapper_Objecte_Text(Markt, "")
+            
+            //TODO: einfügen
+        }
+        // ----------------------------------------------------------------- //
+        // Lager
+        
+        
+        
+        i = i + 1
+    }
+        
+
+
         
     
 }
