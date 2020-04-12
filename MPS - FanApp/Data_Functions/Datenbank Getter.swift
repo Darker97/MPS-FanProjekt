912ce6db1a6530f5c88f3dd7b1ec92df88993a1c
Christian - Darker97 <christian@baltzer.de>
2020-04-12 19:48:54 +0200

    init

---
+++ MPS - FanApp/Data_Functions/Datenbank Getter.swift
@@ -0,0 +1,9 @@
+//
+//  Datenbank Getter.swift
+//  MPS - FanApp
+//
+//  Created by Christian Baltzer on 12.04.20.
+//  Copyright © 2020 Christian Baltzer. All rights reserved.
+//
+
+import Foundation
