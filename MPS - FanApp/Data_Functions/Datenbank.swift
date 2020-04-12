092d98d0104f9614e320195a25083d4555967a15
Christian - Darker97 <christian@baltzer.de>
2020-04-12 12:14:36 +0200

    creating DB

--- MPS - FanApp/Data_Functions/Datenbank.swift
+++ MPS - FanApp/Data_Functions/Datenbank.swift
@@ -1,1 +1,19 @@
 // Funktionen zum analysieren
+
+func openDatabase() -> OpaquePointer? {
+  var db: OpaquePointer?
+
+   //the database file
+   let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+       .appendingPathComponent("HeroesDatabase.sqlite")
+
+   //opening the database
+   if sqlite3_open(fileURL.path, &amp;db) != SQLITE_OK {
+       print("error opening database")
+   }
+
+   //creating table
+    createTables(db)
+    
+    return db
+}
