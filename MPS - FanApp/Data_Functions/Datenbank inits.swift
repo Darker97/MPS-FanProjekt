790fadf971d84e16421ba49ed05509bbd2b74066
Christian - Darker97 <christian@baltzer.de>
2020-06-14 21:04:10 +0200

    Create Tables

--- MPS - FanApp/Data_Functions/Datenbank inits.swift
+++ MPS - FanApp/Data_Functions/Datenbank inits.swift
@@ -26,19 +26,26 @@ func DropAllTables(){
 func createTables(){
     let querrysToCreate =
         ["""
-            CREATE TABLE IF NOT EXISTS `Fest` ( `anfahrt` VARCHAR(255) NULL, `Datum` VARCHAR(255) NOT NULL, `Infotext` VARCHAR(255) NULL, `link` VARCHAR(255) NULL, `name` VARCHAR(255) NOT NULL, PRIMARY KEY (`name`, `Datum`));
-         """,
-         """
-            CREATE TABLE IF NOT EXISTS `Lager` (`Name` VARCHAR(255) NOT NULL,`HomePage` VARCHAR(255) NULL,`Link` VARCHAR(255) NULL,`Fest_name` VARCHAR(255) NOT NULL,PRIMARY KEY (`Name`, `Fest_name`), FOREIGN KEY (`Fest_name`)  REFERENCES `Fest` (`name`)  ON DELETE NO ACTION  ON UPDATE NO ACTION)
-
-         """,
-         """
-            CREATE TABLE IF NOT EXISTS `Band` (`Name` VARCHAR(255) NOT NULL,`typ` VARCHAR(255) NULL,`Zeit` VARCHAR(255) NOT NULL,`Homepage` VARCHAR(255) NULL,`Fest_name` VARCHAR(255) NOT NULL,PRIMARY KEY (`Name`, `Fest_name`, `Zeit`), FOREIGN KEY (`Fest_name`)  REFERENCES `Fest` (`name`)  ON DELETE NO ACTION  ON UPDATE NO ACTION);
-         """,
-         """
-            CREATE TABLE IF NOT EXISTS `Marktstand` (`name` VARCHAR(255) NOT NULL,`Kontakt` VARCHAR(255) NULL,`Homepage` VARCHAR(255) NULL,`Fest_name` VARCHAR(255) NOT NULL,PRIMARY KEY (`name`, `Fest_name`), FOREIGN KEY (`Fest_name`)  REFERENCES `Fest` (`name`)  ON DELETE NO ACTION  ON UPDATE NO ACTION)
-         """
-        ]
+        CREATE TABLE IF NOT EXISTS `Fest` ( `anfahrt` VARCHAR(255) NULL, `Datum` VARCHAR(255) NULL, `Infotext` VARCHAR(255) NULL, `link` VARCHAR(255) NULL, `name` VARCHAR(255) NOT NULL, PRIMARY KEY (`name`))
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS `Lager` ( `Name` VARCHAR(255) NOT NULL, `Beschreibung` VARCHAR(255) NULL, `Link` VARCHAR(255) NULL, PRIMARY KEY (`Name`))
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS `Band` ( `Name` VARCHAR(255) NOT NULL, `typ` VARCHAR(255) NULL, `Zeit` VARCHAR(255) NULL, `Homepage` VARCHAR(255) NULL, `BildLink` VARCHAR(255) NULL, PRIMARY KEY (`Name`))
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS `Marktstand` ( `name` VARCHAR(255) NOT NULL, `Kontakt` VARCHAR(255) NULL, `Homepage` VARCHAR(255) NULL, `BildLink` VARCHAR(255) NULL, PRIMARY KEY (`name`))
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS `Fest_has_Band` ( `Fest_name` VARCHAR(255) NOT NULL, `Band_Name` VARCHAR(255) NOT NULL, PRIMARY KEY (`Fest_name`, `Band_Name`), INDEX `fk_Fest_has_Band_Band1_idx` (`Band_Name` ASC) VISIBLE, INDEX `fk_Fest_has_Band_Fest_idx` (`Fest_name` ASC) VISIBLE, CONSTRAINT `fk_Fest_has_Band_Fest` FOREIGN KEY (`Fest_name`) REFERENCES `Fest` (`name`) ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT `fk_Fest_has_Band_Band1` FOREIGN KEY (`Band_Name`) REFERENCES `Band` (`Name`) ON DELETE NO ACTION ON UPDATE NO ACTION)
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS `Fest_has_Marktstand` ( `Fest_name` VARCHAR(255) NOT NULL, `Marktstand_name` VARCHAR(255) NOT NULL, PRIMARY KEY (`Fest_name`, `Marktstand_name`), INDEX `fk_Fest_has_Marktstand_Marktstand1_idx` (`Marktstand_name` ASC) VISIBLE, INDEX `fk_Fest_has_Marktstand_Fest1_idx` (`Fest_name` ASC) VISIBLE, CONSTRAINT `fk_Fest_has_Marktstand_Fest1` FOREIGN KEY (`Fest_name`) REFERENCES `Fest` (`name`) ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT `fk_Fest_has_Marktstand_Marktstand1` FOREIGN KEY (`Marktstand_name`) REFERENCES `Marktstand` (`name`) ON DELETE NO ACTION ON UPDATE NO ACTION)
+        """,
+        """
+        CREATE TABLE IF NOT EXISTS `Fest_has_Lager` (`Fest_name` VARCHAR(255) NOT NULL,`Lager_Name` VARCHAR(255) NOT NULL,PRIMARY KEY (`Fest_name`, `Lager_Name`),INDEX `fk_Fest_has_Lager_Lager1_idx` (`Lager_Name` ASC) VISIBLE,INDEX `fk_Fest_has_Lager_Fest1_idx` (`Fest_name` ASC) VISIBLE,CONSTRAINT `fk_Fest_has_Lager_Fest1` FOREIGN KEY (`Fest_name`) REFERENCES `Fest` (`name`) ON DELETE NO ACTION ON UPDATE NO ACTION, CONSTRAINT `fk_Fest_has_Lager_Lager1` FOREIGN KEY (`Lager_Name`) REFERENCES `Lager` (`Name`) ON DELETE NO ACTION ON UPDATE NO ACTION)
+        """]
     
     for query in querrysToCreate{
         exeute_withoutReturn(Query: query)
